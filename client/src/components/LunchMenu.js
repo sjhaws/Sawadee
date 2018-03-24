38b4a3c46ce7293d089ace14445104463bd3a8e4
juanaguiniga <juanaguiniga@ymail.com>
2018-03-24 16:54:37 -0600

    removed order button

--- client/src/components/LunchMenu.js
+++ client/src/components/LunchMenu.js
@@ -16,14 +16,6 @@ class LunchMenu extends Component {
       <div className="ui container">
       <br />
       <Header as='h1' textAlign='center'>Lunch Menu</Header>
-      <div>
-        <button class="huge ui button">
-        <a href="https://www.doordash.com/store/sawadee-utah-thai-restaurant-salt-lake-city-130244/?utm_campaign=gpa" target="_blank">
-        Order Now
-        </a>
-        </button>
-      </div>
-      <br />
         <div className="ui grid">
           { this.state.menus.map( menu => 
             <MenuItem {...menu}/>
