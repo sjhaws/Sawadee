a9cbd849d13f205a9a809582932d48cc067cca92
Brock Beuch <brockbeuch@gmail.com>
2018-03-24 16:05:14 -0600

    fixed conflict issues

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
+
 import { Header, Image } from 'semantic-ui-react';
 import logo from '../images/Sawadee-White.png';
-=======
-import { Header } from 'semantic-ui-react';
+
+
 import About from './About';
 
->>>>>>> e0b15bec39ddfb70d2ee374fbe51531a839fd611
+
 
 class Home extends Component {
   render() {
     return (
-<<<<<<< HEAD
+        <div>
         <div className="filtered">
           <div className="heroimg">
             <div className="herologo" centered>
@@ -19,13 +19,14 @@ class Home extends Component {
             </div>
           </div>
         </div>
-=======
+
       <div>
-        <Header as='h1' textAlign='center'>Sawadee Thai</Header>
+
 
         <About />
       </div>
->>>>>>> e0b15bec39ddfb70d2ee374fbe51531a839fd611
+      </div>
+
     );
   }
 }
