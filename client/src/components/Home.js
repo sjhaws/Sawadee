e0b15bec39ddfb70d2ee374fbe51531a839fd611
Tiffany Bigelow <cbtiff@gmail.com>
2018-03-24 15:51:06 -0600

    About Us added to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import About from './About';
+
 
 class Home extends Component {
   render() {
     return (
+      <div>
         <Header as='h1' textAlign='center'>Sawadee Thai</Header>
+
+        <About />
+      </div>
     );
   }
 }
